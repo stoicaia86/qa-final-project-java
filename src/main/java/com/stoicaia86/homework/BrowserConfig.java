6afefe230ff813674f01510774c04bfa0f6f853c
stoicaia86 <stoica_ia@yahoo.com>
2025-11-02 23:07:10 +0200

    Create BrowserConfig.java

---
+++ src/main/java/com/stoicaia86/homework/BrowserConfig.java
@@ -0,0 +1,9 @@
+// Fișier: BrowserConfig.java
+
+public class BrowserConfig {
+    // Câmpuri private
+    private BrowserType browser;
+    private String version;
+    private boolean isHeadless;
+
+}
