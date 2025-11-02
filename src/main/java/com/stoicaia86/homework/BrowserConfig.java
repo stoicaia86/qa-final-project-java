c9faeee92ead006d6d9f50de562ace175d1779c1
stoicaia86 <stoica_ia@yahoo.com>
2025-11-02 23:58:07 +0200

    Update BrowserConfig.java

--- src/main/java/com/stoicaia86/homework/BrowserConfig.java
+++ src/main/java/com/stoicaia86/homework/BrowserConfig.java
@@ -6,4 +6,35 @@ public class BrowserConfig {
     private String version;
     private boolean isHeadless;
 
+
+    public BrowserConfig(BrowserType browser, String version, boolean isHeadless) {
+        this.browser = browser;
+        this.version = version;
+        this.isHeadless = isHeadless;
+
+    }
+
+    public BrowserConfig(BrowserType browser, String version) {
+        this(browser, version, false); // Apelăm primul constructor (chaining)
+    }
+
+    public BrowserConfig(BrowserType browser) {
+        this(browser, "latest", true); // Apelăm al doilea constructor (chaining)
+    }
+
+    //  Metodă statică "Factory"
+    public static BrowserConfig createDefaultChromeConfig() {
+        return new BrowserConfig(BrowserType.CHROME, "latest", true);
+    }
+
+
+public void afiseazaConfig() {
+    System.out.println(
+            "Detalii BrowserConfig:" +
+                    "\n  - Browser: " + this.browser +
+                    "\n  - Version: " + this.version +
+                    "\n  - isHeadless: " + this.isHeadless);
 }
+}
+
+
